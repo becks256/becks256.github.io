c78876b881cfbbd199654c85fe28ad456215b2ef
becks256 <22892728+becks256@users.noreply.github.com>
2023-02-01 03:00:09 -0500

    chore: create barrel index file for assets directory.

---
+++ src/assets/index.js
@@ -0,0 +1,23 @@
+import pluralsightSnip from "./pluralsight.png"
+import certOne from "./cert1.png"
+import certTwo from "./cert2.png"
+import certThree from "./cert3.png"
+import fccSnip from "./fcc.png"
+import soulfolioSnip from "./soulfolio.png"
+import photoshopSnip from "./photoshop.png"
+import myPriceSnip from "./myprice.png"
+import dapperSnip from "./dapper.png"
+import metaSnip from "./meta.png"
+
+export {
+  pluralsightSnip,
+  certOne,
+  certTwo,
+  certThree,
+  fccSnip,
+  soulfolioSnip,
+  photoshopSnip,
+  myPriceSnip,
+  dapperSnip,
+  metaSnip
+}
