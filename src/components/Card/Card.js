3cf2f348607185705798a21856e3db882d1f4037
becks256 <22892728+becks256@users.noreply.github.com>
2023-02-01 02:58:00 -0500

    feat: create Card component.

---
+++ src/components/Card/Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import "./Card.css"
+
+export const Card = ({title, description, children, href, techStack}) => {
+  const headerClasses = "pb-16"
+  return (
+    <div className="Card ProductCard mr-32 mb-32 overflow-hidden">
+      <section className="Card-imageContainer">
+        {children}
+      </section>
+      <section className="px-16">
+        {!!href && <h3 className={headerClasses}><a href={href}>{title}</a></h3>}
+        {!!!href && <h3 className={headerClasses}>{title}</h3>}
+        <p className="Card--description">{description}</p>
+        {techStack && <p className="Card--techStack-description">Tech Stack: {techStack}</p>}
+      </section>
+    </div>
+  )
+} 
