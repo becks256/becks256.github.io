c4333230198638d4bfe71490551e367134970fda
becks256 <22892728+becks256@users.noreply.github.com>
2023-02-04 08:17:56 -0500

    feat: add type prop to enable different card styles.

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,15 +1,16 @@
+import { clsnx } from '@becks256/clsnx'
 import React from 'react';
 import "./Card.css"
 
-export const Card = ({title, description, children, href, techStack}) => {
+export const Card = ({title, description, children, href, techStack, type="product"}) => {
   const headerClasses = "pb-16"
   return (
-    <div className="Card ProductCard sm:mr-0 md:mr-32 mb-32 overflow-hidden">
+    <div className={clsnx({"ProductCard": type === "product", "NetworkCard": type === "network"}, "Card mb-32 overflow-hidden")}>
       <section className="Card-imageContainer">
         {children}
       </section>
       <section className="px-16">
-        {!!href && <h3 className={headerClasses}><a href={href}>{title}</a></h3>}
+        {!!href && <h3 className={headerClasses}><a href={href} target="_blank" rel="noreferrer">{title}</a></h3>}
         {!!!href && <h3 className={headerClasses}>{title}</h3>}
         <p className="Card--description">{description}</p>
         {techStack && <p className="Card--techStack-description">Tech Stack: {techStack}</p>}
