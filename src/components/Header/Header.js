745dc3d3fd40a58f1a8562c7325b5e348d54f3c4
becks256 <22892728+becks256@users.noreply.github.com>
2024-03-19 13:33:08 -0400

    adds smooth scrolling.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,9 +6,10 @@ import "./Header.css"
 
 export const Header = () => {
   const [isMobile, setIsMobile] = useState(false)
+  const ref = React.useRef()
 
   useEffect(() => {
-    /mobile/gi.test(navigator?.userAgent) && setIsMobile(true)
+    ;/mobile/gi.test(navigator?.userAgent) && setIsMobile(true)
     window.innerWidth < 900 && setIsMobile(true)
     window.addEventListener("resize", () => {
       let width = window.innerWidth
@@ -21,7 +22,35 @@ export const Header = () => {
       })
   }, [])
 
+  const clickHandler = (e, item) => {
+    e.preventDefault()
+
+    if (item.href === "/") {
+      window.scrollTo({ top: 0, behavior: "smooth" })
+      return
+    }
+
+    // Select your header and the target element
+    const headerHeight = ref.current.offsetHeight + 50 // Adjust this to your header's selector
+    const targetElement = document.querySelector(item.href)
+
+    if (targetElement) {
+      // Calculate the position you want to scroll to
+      const targetPosition =
+        targetElement.getBoundingClientRect().top +
+        window.pageYOffset -
+        headerHeight
+
+      // Smooth scroll to the calculated position
+      window.scrollTo({
+        top: targetPosition,
+        behavior: "smooth",
+      })
+    }
+  }
+
   const LINK_DATA = [
+    { href: "#publications", innerText: "Publications" },
     { href: "#networks", innerText: "Networks" },
     { href: "#skills", innerText: "Skills" },
     { href: "#work", innerText: "Work" },
@@ -31,10 +60,15 @@ export const Header = () => {
   return isMobile ? (
     <MobileNav links={LINK_DATA} />
   ) : (
-    <nav className="Header">
+    <nav ref={ref} className="Header">
       <DarkmodeToggle className="px-16 pointer flex-no-shrink" />
       {LINK_DATA.map((item, index) => (
-        <a key={`headerlink-${index}`} href={item.href} className="Header-link">
+        <a
+          key={`headerlink-${index}`}
+          href={item.href}
+          className="Header-link"
+          onClick={(e) => clickHandler(e, item)}
+        >
           {item.innerText}
         </a>
       ))}
