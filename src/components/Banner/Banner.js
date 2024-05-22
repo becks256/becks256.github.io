dc25b2faf8714663f5d58bf32dbd11e686368d15
becks256 <22892728+becks256@users.noreply.github.com>
2024-05-22 17:23:21 -0400

    adds scroll hint to banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -7,11 +7,20 @@ export const Banner = () => {
     <>
       <section className="Banner">
         <Header />
+        <div className="Banner--icon">
+          <span class="chevron orange"></span>
+          <span class="chevron red"></span>
+          <span class="chevron lt-purple"></span>
+          <span class="chevron purple"></span>
+          <span class="chevron white"></span>
+        </div>
         <section className="flex flex-column">
           <h1>Hi!</h1>
           <h1>I'm Dan</h1>
           <h2>
-            I love to create tech solutions, like web apps and services. I love it even more when those solutions empower users to do unique & meaningful things.
+            I love to create tech solutions, like web apps and services. I love
+            it even more when those solutions empower users to do unique &
+            meaningful things.
           </h2>
         </section>
       </section>
