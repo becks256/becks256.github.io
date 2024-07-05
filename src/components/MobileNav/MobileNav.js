4af0cbc1fdb770903c5b12eb01b9656be45eba56
becks256 <22892728+becks256@users.noreply.github.com>
2024-07-05 16:57:49 -0400

    makes mobile nav dependent on link data just like header.

--- src/components/MobileNav/MobileNav.js
+++ src/components/MobileNav/MobileNav.js
@@ -1,8 +1,10 @@
 import React from "react"
 import { DarkmodeToggle } from "react-darkmode-hook"
+import { LINK_DATA } from ".."
+
 import "./MobileNav.css"
 
-export const MobileNav = ({links}) => {
+export const MobileNav = () => {
   return (
     <section style={{position: "fixed", top: 0, left: 0, height: "100vh"}}>
       <input type="checkbox" id="hamburger-input" className="burger-shower" />
@@ -15,7 +17,7 @@ export const MobileNav = ({links}) => {
         <nav id="sidebar-menu">
           <h3 className="mt-64 mb-32">Menu</h3>
           <ul>
-          {links.reverse().map((item, index) => (
+          {LINK_DATA.reverse().map((item, index) => (
             <li key={`moblenav-${index}`}>
               <a href={item.href}>
                 {item.innerText}
