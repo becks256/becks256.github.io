bafacee047e75abbf680f331ec61a88ff5b8da14
becks256 <22892728+becks256@users.noreply.github.com>
2024-07-05 16:54:38 -0400

    exports link data from components barrel

--- src/components/index.js
+++ src/components/index.js
@@ -6,3 +6,11 @@ export { Message } from "./Message/Message"
 export { InfoIcon } from "./Icon/Icons/InfoIcon"
 export { Tag } from "./Tag/Tag"
 export { SocialIconBar } from "./SocialIconBar/SocialIconBar"
+
+export const LINK_DATA = [
+  { href: "#publications", innerText: "Publications" },
+  { href: "#skills", innerText: "Skills" },
+  { href: "#projects", innerText: "Projects" },
+  { href: "#networks", innerText: "Networks" },
+  { href: "/", innerText: "Home" },
+]
