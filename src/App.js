39bbd36684112fad3b11db89e73b5e6fdaa69a5b
becks256 <22892728+becks256@users.noreply.github.com>
2024-07-28 22:40:49 -0400

    updates sort and filter methods

--- src/App.js
+++ src/App.js
@@ -91,80 +91,99 @@ function App() {
 
   return (
     <ModalContext.Provider value={{ modalHidden, setModalHidden }}>
-    <div className="App">
-      <Banner isMobile={isMobile} />
-      <section className="main-content sm:mx-16 my-96">
-        <h1 className={headerClasses} id="networks">
-          Networks
-        </h1>
-        <p>Follow my work or connect with me on social media!</p>
-        <section className={cardSectionClasses}>
-          <SocialIconBar />
-        </section>
-        <h1 className={headerClasses} id="projects">
-          Projects
-        </h1>
-        <section className="Card pb-24 flex-column px-24 mb-24 pt-8 gap-8">
-          <p className="font-bold">Filter work by technology</p>
-          <section className="flex flex-row gap-8 flex-wrap">
-            {Array.from(techItems)
-              .sort((a, b) => (a.toLowerCase() > b.toLowerCase() ? 1 : -1))
-              .map((techItem, index) => (
-                <Tag
-                  kind={
-                    filter.some((item) => item.replace(/\\/g, "") === techItem)
-                      ? "special"
-                      : ""
-                  }
-                  key={`tag-${index}`}
-                  onClick={filterHandler}
-                  className="pointer"
-                >
-                  {techItem}
-                </Tag>
-              ))}
+      <div className="App">
+        <Banner isMobile={isMobile} />
+        <section className="main-content sm:mx-16 my-96">
+          <h1 className={headerClasses} id="networks">
+            Networks
+          </h1>
+          <p>Follow my work or connect with me on social media!</p>
+          <section className={cardSectionClasses}>
+            <SocialIconBar />
           </section>
-        </section>
-        <div className="mt-8 mb-24 pl-16">
-          <p className="font-m font-bold gap-8">
-            <Tag kind="info" className="font-bold mr-8">
-              {filteredWork.length}
-            </Tag>
-            Projects match the selected filters
-          </p>
-        </div>
-        <section className={clsnx(cardSectionClasses, "grid-section")}>
-          {filteredWork.length > 0 &&
+          <h1 className={headerClasses} id="projects">
+            Projects
+          </h1>
+          <section className="Card pb-24 flex-column px-24 mb-24 pt-8 gap-8">
+            <p className="font-bold">Filter work by technology</p>
+            <section className="flex flex-row gap-8 flex-wrap">
+              {Array.from(techItems)
+                .sort((a, b) => (a.toLowerCase() > b.toLowerCase() ? 1 : -1))
+                .map((techItem, index) => (
+                  <Tag
+                    kind={
+                      filter.some(
+                        (item) => item.replace(/\\/g, "") === techItem
+                      )
+                        ? "special"
+                        : ""
+                    }
+                    key={`tag-${index}`}
+                    onClick={filterHandler}
+                    className="pointer"
+                  >
+                    {techItem}
+                  </Tag>
+                ))}
+            </section>
+          </section>
+          <div className="mt-8 mb-24 pl-16">
+            <p className="font-m font-bold gap-8">
+              <Tag kind="info" className="font-bold mr-8">
+                {filteredWork.length}
+              </Tag>
+              Projects match the selected filters
+            </p>
+          </div>
+          <section className={clsnx(cardSectionClasses, "grid-section")}>
+            {filteredWork.length > 0 &&
+              filteredWork
+                .sort((a, b) => {
+                  // First criteria: Prioritize items with a 'link' property
+                  if (!!a.link && !b.link) return -1
+                  if (!a.link && !!b.link) return 1
+
+                  // Second criteria: Sort by date in descending order
+                  const dateA = a.date?.match("-")
+                    ? a.date.split("-").pop()
+                    : a.date
+                  const dateB = b.date?.match("-")
+                    ? b.date.split("-").pop()
+                    : b.date
+
+                  // Convert to number for proper comparison (assuming date is in a comparable format)
+                  return new Date(dateB) - new Date(dateA)
+                })
                 .map((item, index) => (
                   <Card key={`work-card-${index}`} data={item} />
-            ))}
-        </section>
-        <h1 className={headerClasses} id="skills">
-          Skills
-        </h1>
-        <section className={clsnx(cardSectionClasses, "grid-section")}>
-          {SKILLS_DATA.sort((a, b) =>
-            a.title.toLowerCase() < b.title.toLowerCase() ? -1 : 1
-          ).map((item, index) => (
+                ))}
+          </section>
+          <h1 className={headerClasses} id="skills">
+            Skills
+          </h1>
+          <section className={clsnx(cardSectionClasses, "grid-section")}>
+            {SKILLS_DATA.sort((a, b) =>
+              a.title.toLowerCase() < b.title.toLowerCase() ? -1 : 1
+            ).map((item, index) => (
               <Card key={`work-card-${index}`} data={item} />
-          ))}
-        </section>
-        <h1 className={headerClasses} id="publications">
-          Publications
-        </h1>
-        <section className={clsnx(cardSectionClasses, "grid-section")}>
-          {PUBLICATION_DATA.map((item, index) => (
+            ))}
+          </section>
+          <h1 className={headerClasses} id="publications">
+            Publications
+          </h1>
+          <section className={clsnx(cardSectionClasses, "grid-section")}>
+            {PUBLICATION_DATA.map((item, index) => (
               <Card key={`work-card-${index}`} data={item} type="publication" />
-          ))}
+            ))}
+          </section>
         </section>
-      </section>
-      <footer className="m-32 flex align-content--center justify-content--center">
-        <p className="font-s">
-          All content contained herein is the property of Daniel M Becker.
-          Copyright &copy; 2023 &mdash; {APP_DATA.year}. All rights reserved.
-        </p>
-      </footer>
-    </div>
+        <footer className="m-32 flex align-content--center justify-content--center">
+          <p className="font-s">
+            All content contained herein is the property of Daniel M Becker.
+            Copyright &copy; 2023 &mdash; {APP_DATA.year}. All rights reserved.
+          </p>
+        </footer>
+      </div>
     </ModalContext.Provider>
   )
 }
