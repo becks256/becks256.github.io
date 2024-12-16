7ab3ec92f89ffc81f7cad705bfc289fdf79886c4
becks256 <22892728+becks256@users.noreply.github.com>
2024-12-16 01:37:20 -0500

    organize into components and patterns

--- src/components/index.js
+++ src/components/index.js
@@ -1,21 +1,6 @@
-export { Banner } from "./Banner/Banner"
-export { Header } from "./Header/Header"
 export { Card } from "./Card/Card"
-export { MobileNav } from "./MobileNav/MobileNav"
 export { Message } from "./Message/Message"
 export { InfoIcon } from "./Icon/Icons/InfoIcon"
 export { IconClose } from "./Icon/Icons/IconClose"
 export { Tag } from "./Tag/Tag"
-export { SocialIconBar } from "./SocialIconBar/SocialIconBar"
-export { default as WebGLBanner } from "./WebGLBanner"
 export { Modal } from "./Modal/Modal"
-export { WorkWithMeSection } from "./WorkWithMeSection"
-
-export const LINK_DATA = [
-  { href: "#publications", innerText: "Publications" },
-  { href: "#skills", innerText: "Skills" },
-  { href: "#projects", innerText: "Projects" },
-  { href: "#networks", innerText: "Networks" },
-  // { href: "#about", innerText: "About Me" },
-  // { href: "/", innerText: "Home" },
-]
---
+++ src/patterns/index.js
@@ -0,0 +1,8 @@
+import { HoverEffect } from "./HoverEffect"
+
+export { Header } from "./Header/Header"
+export { HoverEffect } from "./HoverEffect"
+export { MobileNav } from "./MobileNav/MobileNav"
+export { SocialIconBar } from "./SocialIconBar/SocialIconBar"
+export { default as WebGLBanner } from "./WebGLBanner/WebGLBanner"
+export { WorkWithMeSection } from "./WorkWithMeSection/WorkWithMeSection"
